d7d2d8d64aae91c4e5eb37977780710988f63c1c
anasM0hammad <anas.1633.m@gmail.com>
2018-11-25 00:40:07 +0530

    Email Client Complete

--- Email Client/send_mail.php
+++ Email Client/send_mail.php
@@ -1,6 +1,24 @@
 <?php 
 
-echo json_encode('success') ;
+if(isset($_GET['to']) && isset($_GET['from'])){
+
+$to = $_GET['to'];
+$from = $_GET['from'];
+$subject = $_GET['subject'];
+$message = $_GET['message'];
+
+ $isDone = mail($to , $subject , $message , $from);
+
+if($isDone){
+	echo json_encode('success');
+}
+else{
+	echo json_encode('unsuccess');
+}
+
+
+
+}
 
 
 ?>
